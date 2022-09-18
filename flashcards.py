508549f3ea2f57370cead5739abe76f598f94189
manan883 <manan883@gmail.com>
2022-09-17 21:19:58 -0700

    First commit

---
+++ flashcards.py
@@ -0,0 +1,60 @@
+from sys import *
+from os import *
+'''
+ask user to enter question and answer
+put in a map
+store question and answer in a csv file with a , as a seperator 
+when quizzing user the comma as a parse tool and move the data from the csv file into a map when launching.
+
+'''
+def makedir(name):
+    current_directory = getcwd()
+    final_directory = path.join(current_directory, name)
+    if not path.exists(final_directory):
+        makedirs(final_directory)
+    else:
+        return False
+def writeToFile(fileName,dataDict):
+    pass
+def newSet(name):
+    newSetDic = {
+        "questions": [],
+        "answers": []
+    }
+    val = makedir(name)
+    if val == False:
+        print("directory exists already")
+        return 
+    while True:
+        ipt = input("A: Enter a question you want to add to the library:\nB: quit\n")
+        match ipt.upper():
+            case "A":
+                qt = input("Enter the question: ")
+                if qt == "" or None:
+                    print("Question can not be empty")
+                newSetDic["questions"].append(qt)
+                aw = input(("Enter the answer for the question\n",qt + ": "))
+                if aw == "" or None:
+                    print("Answer can not be empty")
+                newSetDic["answers"].append(aw)
+                print(newSetDic)
+            case "B":
+                break
+        
+
+def inputs():
+    while True:
+        userInput = input("Select and option:\nA: new set of cards\nB:study and old set\nC:update and old set\nQ: quit\n")
+        match userInput.upper():
+            case "A":
+                newSet("name")
+            case "B":
+                print
+            case "C":
+                print
+            case "Q":
+                exit()
+            case _:
+                print("Please select a valid input: ")
+
+inputs()               
